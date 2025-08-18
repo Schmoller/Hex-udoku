afdad71c8442ef31e4b4a1995a47af3706d4fc92
Steven Schmoll <steven.schmoll@gmail.com>
2025-08-18 22:33:35 +1000

    Attempt at adding PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,41 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: [
+                'favicon.ico',
+                'favicon.svg',
+                'icons/apple-touch-icon.png',
+                'icons/icon-192.png',
+                'icons/icon-512.png',
+            ],
+            manifest: {
+                name: 'Hex-udoku',
+                short_name: 'Hex-udoku',
+                description: 'A hexagonal Sudoku game',
+                theme_color: '#E0E7FF',
+                icons: [
+                    {
+                        src: 'icons/icon-192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'icons/icon-512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                ],
+            },
+        }),
+    ],
 });
