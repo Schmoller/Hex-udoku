94a20d9f87e62ad73de65501803e69ff32906433
Steven Schmoll <steven.schmoll@gmail.com>
2025-08-19 21:17:31 +1000

    Fix hyphen

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -7,7 +7,7 @@ interface MainLayoutProps {
 export const MainLayout: FC<MainLayoutProps> = ({ children }) => {
     return (
         <div className="w-lvw h-lvh flex flex-col items-stretch">
-            <div className="p-4 bg-gray-600 text-blue-100 text-2xl">Hexudoku</div>
+            <div className="p-4 bg-gray-600 text-blue-100 text-2xl">Hex-udoku</div>
             <div className="flex-grow p-6 flex flex-col items-center justify-center">{children}</div>
         </div>
     );
