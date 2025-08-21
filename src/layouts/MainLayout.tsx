98556a2636e8c407b63861f0aac6043f0648ee17
Steven Schmoll <steven.schmoll@gmail.com>
2025-08-21 19:09:31 +1000

    Improve navbar

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -7,7 +7,10 @@ interface MainLayoutProps {
 export const MainLayout: FC<MainLayoutProps> = ({ children }) => {
     return (
         <div className="w-lvw h-lvh flex flex-col items-stretch">
-            <div className="p-4 bg-gray-600 text-blue-100 text-2xl">Hex-udoku</div>
+            <div className="navbar bg-base-200 shadow-sm flex flex-row gap-2">
+                <img src="/icons/icon-512.png" className="w-12 h-12" />
+                hex-udoku
+            </div>
             <div className="flex-grow p-6 flex flex-col items-center justify-center">{children}</div>
         </div>
     );
